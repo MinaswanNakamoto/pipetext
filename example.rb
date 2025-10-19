150acf849d920f87a49f2dbc8d8a47629a0d58f5
MinaswanNakamoto <minaswan.nakamoto@onionmail.org>
2025-10-19 12:32:49 +0000

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'pipetext'
+
+class Alice
+  include PipeText
+  def say(string)
+    write("Alice says, '|y#{string}|n'")
+  end
+end
+
+class Bob
+  include PipeText
+  def say(string)
+    write("Bob says, '|m#{string}|n'")
+  end
+end
+
+class Carol
+  include PipeText
+  def say(string)
+    # Defaults to using & for BG colors so no need for |&
+    paint("Carol says, '|W&g#{string}&n|n'")
+  end
+end
+
+alice = Alice.new
+bob = Bob.new
+carol = Carol.new
+alice.say("Hello world!")
+bob.say("Oh.. hi Alice! |&|g&rAnd goodbye world time to exit!&n|n")
+carol.say("Hey Alice. Bob, you are so melodramatic. Let's transact later.")
+pipe = Class.new.extend(PipeText)
+pipe.write('Check = |G|U+2714|n, Cross = |R|U+274C |n')
+exit!
